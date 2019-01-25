8186b6d29278ada5fc9a0907f162937f1fee2e67
Sachet <sachet.mittal@gmail.com>
2019-01-24 16:56:30 -0800

    WIP: minor changes to previous commit

--- get_feelings.py
+++ get_feelings.py
@@ -1,32 +1,40 @@
+from enum import Enum
+
 import pandas as pd
 
-# Data source
-filename = r"data\static_annotations_averaged_songs_2000_2058.csv"
 
-df = pd.read_csv(filename)
+def main():
+    # Data source
+    filename = r"data\static_annotations_averaged_songs_2000_2058.csv"
 
-# Remove unneccesay columns
-col_info = list(enumerate(df.columns))
-df.drop(columns=[col_info[i][1] for i in range(2,7)], inplace=True)
+    df = pd.read_csv(filename)
 
-col_info = list(enumerate(df.columns))
-df.drop(columns=[col_info[i][1] for i in range(3,8)], inplace=True)
+    # Remove unneccesay columns
+    col_info = list(enumerate(df.columns))
+    df.drop(columns=[col_info[i][1] for i in range(2,7)], inplace=True)  # Valence related columns
 
-# Rename columns
-df.rename(columns = {' valence_mean':'valence'}, inplace = True)
-df.rename(columns = {' arousal_mean':'arousal'}, inplace = True)
+    col_info = list(enumerate(df.columns))
+    df.drop(columns=[col_info[i][1] for i in range(3,8)], inplace=True)  # Arousal related columns
 
-# Mean Normalize values
-df['valence'] = df['valence'] - 5
-df['arousal'] = df['arousal'] - 5
+    # Rename columns
+    df.rename(columns = {' valence_mean':'valence'}, inplace = True)
+    df.rename(columns = {' arousal_mean':'arousal'}, inplace = True)
 
-# Add a new column
-Feelings = {0:"Happy", # Valence > 0 and Arousal >0 
-        1: "Sad",  # Valence > 0 and Arousal < 0
-        2: "Angry",  # Valence <0 and Arousal > 0
-        4: "Calm"   # Valence < 0 and Arousal < 0
-}
-df["Feeling"] = 1
-df["Feeling"] = df['valence'] > 0 & df['arousal'] > 0 
-df.head()
-import pdb; pdb.set_trace()
+    # Mean Normalize values
+    df['valence'] = df['valence'] - 5
+    df['arousal'] = df['arousal'] - 5
+
+    Feelings = ["Happy", # Valence > 0 and Arousal >0 
+        "Sad",  # Valence > 0 and Arousal < 0
+        "Angry",  # Valence <0 and Arousal > 0
+        "Calm"   # Valence < 0 and Arousal < 0
+    ]
+
+    # Add a new column
+    df["Feeling"] = 1
+    df["Feeling"] = df['valence'] > 0 & df['arousal'] > 0 
+    print(df.head())
+
+
+if __name__ == "__main__":
+    main()
