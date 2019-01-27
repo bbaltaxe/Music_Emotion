395382ebcacefd0a500da8df0e2c2221c15eecb1
Sachet <sachet.mittal@gmail.com>
2019-01-26 20:53:20 -0800

    Script to get retrive feelings from the dataset
    This is a sample code and most probably this will be remove/re-designed.

--- get_feelings.py
+++ get_feelings.py
@@ -1,12 +1,9 @@
-from enum import Enum
-
 import pandas as pd
 
 
 def main():
     # Data source
     filename = r"data\static_annotations_averaged_songs_2000_2058.csv"
-
     df = pd.read_csv(filename)
 
     # Remove unneccesay columns
@@ -24,15 +21,26 @@ def main():
     df['valence'] = df['valence'] - 5
     df['arousal'] = df['arousal'] - 5
 
+    def feeling_map(s):
+        '''Refer: https://stackoverflow.com/questions/49586471/add-new-column-to-python-pandas-dataframe-based-on-multiple-conditions
+        open to new ideas for this'''
+        if s['valence'] >= 0 and s['arousal'] >= 0:
+            return 0 
+        if s['valence'] >= 0 and s['arousal'] < 0:
+            return 1
+        if s['valence'] < 0 and s['arousal'] >= 0:
+            return 2 
+        if s['valence'] < 0 and s['arousal'] < 0:
+            return 3
+
     Feelings = ["Happy", # Valence > 0 and Arousal >0 
-        "Sad",  # Valence > 0 and Arousal < 0
+        "Calm"   # Valence > 0 and Arousal < 0
         "Angry",  # Valence <0 and Arousal > 0
-        "Calm"   # Valence < 0 and Arousal < 0
+        "Sad",  # Valence < 0 and Arousal < 0
     ]
 
     # Add a new column
-    df["Feeling"] = 1
-    df["Feeling"] = df['valence'] > 0 & df['arousal'] > 0 
+    df["Feeling"] = df.apply(feeling_map, axis=1)
     print(df.head())
 
 
