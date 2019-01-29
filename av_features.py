686ccd363d49084cc3de609c0a8ff8c2a7ab84c2
Breanna Baltaxe-Admony <baltaxeadmony@gmail.com>
2019-01-28 18:03:53 -0800

    script to get feature averages

--- av_features.py
+++ av_features.py
@@ -7,10 +7,12 @@ import pandas as pd
 
 def main(): 
 
+    #create new data frame from existing cols
     df = pd.read_csv("~/Downloads/features/10.csv",sep=';')
     df = df.filter(regex='mean',axis=1)
     adf = pd.DataFrame(columns=df.columns)
     
+    #iterate over all song csvs
     for n in range(2060):
         filename = "~/Downloads/features/{}.csv".format(n)
     
@@ -19,13 +21,9 @@ def main():
         except FileNotFoundError: 
             print("no file here, sir")
             continue
+
         #only use columns that include the mean
         df = df.filter(regex='mean',axis=1)
-        #df = df.iloc[:,0]
-        #df = pd.concat([df,ndf],axis=1)
-   
-        #create new dataframe 
-        #adf = pd.DataFrame(columns=df.columns)
     
         #add a row for each csv
         #each col is the av of the full csv
@@ -36,6 +34,7 @@ def main():
     
         print(adf.info())
     
+    #save new df as csv
     adf.to_csv("out.csv")
     
     return 
